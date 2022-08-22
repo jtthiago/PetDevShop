e752ee96b0b662e51a49a7243ae807455c26ca84
Thiago Haubrick <69656413+jtthiago@users.noreply.github.com>
2022-08-22 13:55:34 -0300

    Add files via upload

---
+++ src/controllers/pageController.ts
@@ -0,0 +1,59 @@
+import { Request, Response } from "express";
+import { createMenuObject } from '../helpers/createMenuObject';
+
+import { Pet } from "../models/pet";
+
+export const home = (_req: Request, res: Response)=> {
+    let list = Pet.getAll
+
+    res.render('pages/page', {
+        menu: createMenuObject('all'),
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        },
+        list
+    });
+    
+}
+
+export const dogs = (_req: Request, res: Response) => {
+    let list = Pet.getFromType('dog');
+
+    res.render('pages/page', {
+        menu: createMenuObject('dog'),
+        banner: {
+            title: 'Cachorros',
+            background: 'banner_dog.jpg'
+        },
+        list
+    });
+
+    
+}
+
+export const cats = (_req: Request, res: Response) => {
+    let list = Pet.getFromType('cat');
+
+    res.render('pages/page', {
+        menu: createMenuObject('cat'),
+        banner: {
+            title: 'Gatos',
+            background: 'banner_cat.jpg'
+        },
+        list
+    });
+}
+
+export const fishes = (_req: Request, res: Response) => {
+    let list = Pet.getFromType('fish');
+
+    res.render('pages/page', {
+        menu: createMenuObject('fish'),
+        banner: {
+            title: 'Peixes',
+            background: 'banner_fish.jpg'
+        },
+        list
+    });
+}
